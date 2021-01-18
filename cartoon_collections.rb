86b7bcfbb9b641ba882730aed9ff9dc9991939c6
Justin Movitz <movitzj1@montclair.edu>
2021-01-18 12:55:47 -0500

    commit tree 687793284c694ee5ec7543c1c965e1112b8e23cf

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#require 'pry'
+#dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+#planeteer_calls = ["earth", "wind", "fire", "water", "heart"] 
+
+def roll_call_dwarves(array)
+  array.map.each.with_index(1) do |element, index|
+    puts "#{index}. #{element}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |element|
+    p element.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |element|
+    if element.length > 4
+      true
+    else
+      false
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  cheeses = ["cheddar", "gouda", "camembert"]
+  array.detect do |element|
+    if cheeses.include?(element)
+      return element
+    end
+  end
 end
